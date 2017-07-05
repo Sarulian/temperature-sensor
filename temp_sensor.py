bee4a1d5f7aec5ce1a6bdfa84e6aa02f7a19e315
Sarulian <tedjamesrobinson@gmail.com>
2017-07-04 21:07:12 -0700

    added darksky integration

--- temp_sensor.py
+++ temp_sensor.py
@@ -6,11 +6,12 @@ import os
 import csv
 
 
-def make_request(location):
-	if location == "outdoor":
-		return "https://api.darksky.net/forecast/{0}/{1},{2}".format(config.key, config.gps_coords[0], config.gps_coords[1])
-	if location == "indoor":
-		return "http://{0}/weather".format(config.esp_ip)
+def make_outdoor_request():
+	return "https://api.darksky.net/forecast/{0}/{1},{2}".format(config.key, config.gps_coords[0], config.gps_coords[1])
+
+
+def make_indoor_request(value):
+	return "http://{0}/{1}".format(config.esp_ip, value)
 
 
 def write_to_file(date, time, weather):
@@ -21,15 +22,9 @@ def write_to_file(date, time, weather):
 
 if __name__ == "__main__":
 
-	# outdoor_weather = requests.get(make_request("outdoor")).json()
+	outdoor_weather = requests.get(make_outdoor_request()).json()
 
-	# print("Outdoor temperature:")
-	# print(outdoor_weather["currently"]["temperature"])
-
-	# print("Outdoor humidity:")
-	# print(outdoor_weather["currently"]["humidity"])
-
-	# indoor_weather = requests.get(make_request("indoor")).json()
+	# indoor_temp = requests.get(make_indoor_request("temp")).json()
 
 	# print("Indoor temperature:")
 	# print(indoor_weather["temperature"])
@@ -39,20 +34,8 @@ if __name__ == "__main__":
 
 	current_time = time.localtime()
 
-	# year = time.strftime("%Y", current_time)
-	# month = time.strftime("%b", current_time)
-	# weekday = time.strftime("%a", current_time)
-	# day = time.strftime("%d", current_time)
-	# hour = time.strftime("%H", current_time)
-	# minute = time.strftime("%M", current_time)
-	# print("{0}:{1}:00 {2}, {3} {4} {5}".format(hour, minute, weekday, month, day, year))
-
 	date = time.strftime("%m_%d_%Y", current_time)
 	time = time.strftime("%H:%M")
-	weather = [77,54,82,49]
-
-	print(date)
-	print(time)
-	print(weather)
+	weather = [outdoor_weather["currently"]["temperature"],outdoor_weather["currently"]["humidity"],82,49]
 
 	write_to_file(date, time, weather)
