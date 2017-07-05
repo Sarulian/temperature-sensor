d91743ee5b03515eb0cfcd06f142fa43fa391638
Sarulian <tedjamesrobinson@gmail.com>
2017-07-05 08:40:01 -0700

    moving file path to config

--- temp_sensor.py
+++ temp_sensor.py
@@ -15,10 +15,9 @@ def make_indoor_request(value):
 
 
 def write_to_file(date, time, weather):
-	relative_path = '~/Github/temperature-sensor/temp_data/' + date + '.csv'
-	global_path = os.path.abspath(relative_path)
-	print(global_path)
-	with open(global_path, 'a', newline='') as csvfile:
+	path = config.data_path + date + '.csv'
+	print(path)
+	with open(path, 'a', newline='') as csvfile:
 		csv_writer = csv.writer(csvfile, delimiter=',')
 		csv_writer.writerow([time, weather[0], weather[1], weather[2], weather[3]])
 
